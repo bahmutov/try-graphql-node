50541ee9188660850fc6ca64623fa7501bbc5c3e
Gleb Bahmutov <gleb.bahmutov@gmail.com>
2017-06-16 16:44:17 +0200

    roll dice and quote of the day

--- index.js
+++ index.js
@@ -5,14 +5,22 @@ var graphqlHTTP = require('express-graphql');
 // Construct a schema, using GraphQL schema language
 var schema = buildSchema(`
   type Query {
-    hello: String
+    quoteOfTheDay: String
+    random: Float!
+    rollThreeDice: [Int]
   }
 `);
 
 // The root provides a resolver function for each API endpoint
 var root = {
-  hello: () => {
-    return 'Hello world!';
+  quoteOfTheDay: () => {
+    return Math.random() < 0.5 ? 'Take it easy' : 'Salvation lies within';
+  },
+  random: () => {
+    return Math.random();
+  },
+  rollThreeDice: () => {
+    return [1, 2, 3].map(_ => 1 + Math.floor(Math.random() * 6));
   },
 };
 
